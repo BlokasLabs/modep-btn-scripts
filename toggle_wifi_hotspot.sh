2bad1d839b228ddd1a556a3df982000d85cd3bd7
Giedrius <giedrius@blokas.io>
2018-08-31 00:52:57 +0300

    Customized WiFi hotspot togggle script for MODEP

---
+++ toggle_wifi_hotspot.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+# pisound-btn daemon for the Pisound button.
+# Copyright (C) 2017  Vilniaus Blokas UAB, https://blokas.io/pisound
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; version 2 of the
+# License.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+#
+
+. /usr/local/pisound/scripts/common/common.sh
+
+log "Pisound button triple clicked!"
+flash_leds 1
+
+if ps -e | grep -q hostapd; then
+        log "Disabling Access point..."
+        sh /usr/local/pisound/scripts/pisound-btn/disable_wifi_hotspot.sh
+        systemctl stop touchosc
+
+        flash_leds 20
+        sleep 0.5
+        flash_leds 20
+else
+        log "Enabling Access point..."
+        sh /usr/local/pisound/scripts/pisound-btn/enable_wifi_hotspot.sh
+        systemctl start touchosc
+
+        flash_leds 20
+fi
+
